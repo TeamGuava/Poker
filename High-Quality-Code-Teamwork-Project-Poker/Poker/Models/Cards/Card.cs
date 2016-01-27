ea1f482f6a3ccdce8a43cf797a6b5b3f4654b4c0
Aleksandar Tanev <aleksander.tanev.51@gmail.com>
2016-01-27 02:01:38 +0200

    Made some small improvements and documentation ot classes Card and DeckOfCards.

--- High-Quality-Code-Teamwork-Project-Poker/Poker/Models/Cards/Card.cs
+++ High-Quality-Code-Teamwork-Project-Poker/Poker/Models/Cards/Card.cs
@@ -5,17 +5,27 @@
 
     using Poker.Contracts;
 
+    /// <summary>
+    /// Represents the object Card.
+    /// </summary>
     public class Card : ICard
     {
         private string name;
         private Image image;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Card" /> class.
+        /// </summary>
         public Card(string name, string imageLocation)
         {
             this.Name = name;
             this.Image = Image.FromFile(imageLocation); 
         }
 
+        /// <summary>
+        /// Represents the name of the object.
+        /// </summary>
+        /// <value>The Name property gets/sets the value of the field name.</value>
         public string Name
         {
             get
@@ -35,6 +45,10 @@
             }
         }
 
+        /// <summary>
+        /// Represents the image of the object.
+        /// </summary>
+        /// <value>The Image property gets/sets the value of the field image.</value>
         public Image Image
         {
             get
--- High-Quality-Code-Teamwork-Project-Poker/Poker/Models/Cards/DeckOfCards.cs
+++ High-Quality-Code-Teamwork-Project-Poker/Poker/Models/Cards/DeckOfCards.cs
@@ -9,11 +9,23 @@
     public class DeckOfCards : IDeckOfCards, ICardDealer
     {
         private IList<Card> listOfCards;
-        private string locationOfTheCards;
-        private string extentionOfTheImages;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DeckOfCards" /> class.
+        /// </summary>
         public DeckOfCards(string directory, string extention)
         {
+            if (directory == null)
+            {
+                throw new ArgumentNullException("Cannot provide an empty path to the cards directory.");
+            }
+
+            if (extention == null)
+            {
+                throw new ArgumentNullException(
+                    "Cannot provide an empty extension of the card file images.");
+            }
+
             this.ListOfCards = this.LoadCardsFromDirectory(directory, extention);
         }
 
@@ -26,16 +38,25 @@
 
             private set
             {
+                if (value == null)
+                {
+                    throw new ArgumentNullException(
+                        "No list of cards was provided.");
+                }
+
                 if (value.Count == 0)
                 {
                     throw new ArgumentOutOfRangeException(
-                        "No cards were found to be loaded.");
+                        "A deck of cards cannot have 0 cards.");
                 }
 
                 this.listOfCards = value;
             }
         }
-
+      
+        /// <summary>
+        /// Randomly shuffles the cards in the deck.
+        /// </summary>
         public void ShuffleDeck()
         {
             Random random = new Random();
@@ -49,12 +70,15 @@
             }
         }
 
+        /// <summary>
+        /// Draw one card from the deck. This process permanently removes the card from the deck.
+        /// </summary>
+        /// <returns></returns>
         public ICard DrawOneCard()
         {
             if (this.listOfCards.Count == 0)
             {
-                // TODO: Create exception OutOfCardsException.
-                throw new Exception("OutOfCards");
+                throw new ArgumentOutOfRangeException("No cards are left in the deck!");
             }
 
             Card drawnCard = this.listOfCards[0];
@@ -63,44 +87,11 @@
             return drawnCard;
         }
 
-        private string LocationOfTheCards
-        {
-            get
-            {
-                return this.locationOfTheCards;
-            }
-
-            set
-            {
-                if (value == null)
-                {
-                    throw new ArgumentNullException(
-                        "Cannot provide an empty path to the cards directory.");
-                }
-
-                this.locationOfTheCards = value;
-            }
-        }
-
-        private string ExtentionOfTheImages
-        {
-            get
-            {
-                return this.extentionOfTheImages;
-            }
-
-            set
-            {
-                if (value == null)
-                {
-                    throw new ArgumentNullException(
-                        "Cannot provide an empty extension of the card file images.");
-                }
-
-                this.extentionOfTheImages = value;
-            }
-        }
-
+        /// <summary>
+        /// Extracts the card name from the location where it can be found.
+        /// </summary>
+        /// <param name="location">Location of the card.</param>
+        /// <returns>The name of the card from a given location.</returns>
         private string ExtractCardNameFromLocation(string location)
         {
             string name = location;
@@ -118,6 +109,12 @@
             return name;
         }
 
+        /// <summary>
+        /// Loads a list of cards from a given location.
+        /// </summary>
+        /// <param name="directory">Directory in which the cards are located.</param>
+        /// <param name="extention">Extension of the files, which represent the cards.</param>
+        /// <returns>List of cards.</returns>
         private IList<Card> LoadCardsFromDirectory(
             string directory, 
             string extention)
